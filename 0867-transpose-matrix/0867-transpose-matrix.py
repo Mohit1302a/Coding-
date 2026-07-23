1f545f0e552acedab87ce5018b600a04a46c6996
abhinendra pratap <123157777+Mohit1302a@users.noreply.github.com>
2026-07-23 15:02:12 +0530

    Time: 0 ms (100%), Space: 12.9 MB (42.89%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.py
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -1,5 +1,5 @@
 class Solution(object):
     def transpose(self, matrix):
-        return [list(row) for row in zip(*matrix)]
+        return [list(row) for row in zip(*matrix)] #zip(*matrix) groups elements by their column position.
       
         
