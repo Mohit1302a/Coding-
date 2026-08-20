5e639383afd574d62aa659b36556e1b8911c84ca
abhinendra pratap <123157777+Mohit1302a@users.noreply.github.com>
2026-08-20 16:18:16 +0530

    Time: 27 ms (59.5%), Space: 19.1 MB (84.79%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        arr=[]
+        
+        for i in range(1,n+1):
+            if i%5==0 or i%3==0 or i%7==0:
+                arr.append(i)
+        return sum(arr)
